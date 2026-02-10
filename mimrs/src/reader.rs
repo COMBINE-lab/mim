99c1755907026b8e1c938d963fd119f9711cbfd8
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-02-10 18:25:54 +0100

    [rs/reader] Skip hash check at startup; use BufWriter for hash check

--- mimrs/src/reader.rs
+++ mimrs/src/reader.rs
@@ -1,6 +1,7 @@
 use crate::gzip_reader::GzipStreamReader;
 use crate::types::MimIndex;
 use anyhow::Result;
+use std::io::BufWriter;
 use std::ops::Range;
 use std::path::{Path, PathBuf};
 use tracing::debug;
@@ -41,18 +42,18 @@ impl MimReader {
     pub fn new_with_index(gz_path: &Path, index_path: &Path, num_workers: usize) -> Self {
         let index = MimIndex::read_path(&index_path).expect("failed to load index");
 
-        {
-            let hash = hash_gz_file(gz_path);
-            if hash != index.input_hash {
-                panic!(
-                    "Input file {} hash does not match index hash in {}. Expected {:?}, got {:?}.",
-                    gz_path.display(),
-                    index_path.display(),
-                    index.input_hash,
-                    hash
-                );
-            }
-        }
+        // {
+        //     let hash = hash_gz_file(gz_path);
+        //     if hash != index.input_hash {
+        //         panic!(
+        //             "Input file {} hash does not match index hash in {}. Expected {:?}, got {:?}.",
+        //             gz_path.display(),
+        //             index_path.display(),
+        //             index.input_hash,
+        //             hash
+        //         );
+        //     }
+        // }
 
         Self {
             num_workers,
@@ -125,7 +126,11 @@ pub fn hash_gz_file(gz_path: &Path) -> [u8; 32] {
     debug!("hashing input file");
     let mut hasher = blake3::Hasher::new();
     let mut reader = std::fs::File::open(gz_path).expect("could not open gzip file for hashing");
-    std::io::copy(&mut reader, &mut hasher).expect("could not hash gzip file");
+    std::io::copy(
+        &mut reader,
+        &mut BufWriter::with_capacity(256 * 1024, &mut hasher),
+    )
+    .expect("could not hash gzip file");
     debug!("hashing done");
     let hash = *hasher.finalize().as_bytes();
     hash
